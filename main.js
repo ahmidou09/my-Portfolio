e651255eb4bacf01e7d14c1acc5aa93fa0b530ad
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-06-19 23:35:15 +0100

    Edit html & css files and Implement mobile menu functionality for js

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-const menu = document.querySelector(".toggleBtn");
-const nav = document.querySelector(".nav");
+const nav = document.querySelector('.nav');
+const toggelBtn = document.querySelector('.toggel-btn');
+const navItem = document.querySelectorAll('.nav-item');
+const menuIcon = document.querySelector('.menu-icon');
 
-const menuIcon = document.querySelector(".menu-icon");
+toggelBtn.addEventListener('click', () => {
+  nav.classList.toggle('nav-active');
 
-menuIcon.addEventListener("click", function () {
-  console.log("hello");
+  if (nav.classList.contains('nav-active')) {
+    menuIcon.src = './img/icon/close-icon.svg';
+  }
+
+  if (!nav.classList.contains('nav-active')) {
+    menuIcon.src = './img/icon/menu.svg';
+  }
+});
+
+navItem.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    nav.classList.remove('nav-active');
+
+    if (nav.classList.contains('nav-active')) {
+      menuIcon.src = './img/icon/close-icon.svg';
+    }
+
+    if (!nav.classList.contains('nav-active')) {
+      menuIcon.src = './img/icon/menu.svg';
+    }
+  });
 });
